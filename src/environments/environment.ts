7bb9a75239f6b5241d5c35606811d8ef7d58db17
Cyrille83n <93023608+Cyrille83n@users.noreply.github.com>
2022-06-02 17:44:52 +0100

    interface_angular_firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCWhclG79cBvmMYOg-uWxLZsT8p84Eu8S4",
+    authDomain: "cvinterface.firebaseapp.com",
+    projectId: "cvinterface",
+    storageBucket: "cvinterface.appspot.com",
+    messagingSenderId: "100545853901",
+    appId: "1:100545853901:web:bcd0f2a8fc054748c36c84",
+    measurementId: "G-YCX5PLFBCR"
+  }
 };
 
 /*
